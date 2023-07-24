1a2befd0b252c640664dfd79c614d6ca7ae0b010
Addo.Zhang <duwasai@gmail.com>
2023-07-24 10:09:18 +0800

    update manifest generator to support versioning

--- manifest-generator.sh
+++ manifest-generator.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+rm -f manifests/book*.yaml
+
 generate_yaml() {
+    project_name=$(echo $1 | cut -d "-" -f 1)
 cat <<EOF
 apiVersion: v1
 kind: ServiceAccount
 metadata:
-  name: $1
+  name: $project_name
 ---
 apiVersion: v1
 kind: Service
 metadata:
   name: $1
+  labels:
+    version: ${2:-v1}
 spec:
   selector:
     app: $1
+    version: ${2:-v1}
   ports:
     - protocol: TCP
       port: 14001
@@ -23,25 +29,31 @@ apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: $1
+  labels:
+    version: ${2:-v1}
 spec:
   replicas: 1
   selector:
     matchLabels:
       app: $1
+      version: ${2:-v1}
   template:
     metadata:
       labels:
         app: $1
+        version: ${2:-v1}
     spec:
-      serviceAccountName: $1
+      serviceAccountName: $project_name
       containers:
         - name: $1
-          image: addozhang/${1}:latest
+          image: addozhang/${project_name}:latest
           ports:
             - containerPort: 14001
           env:
             - name: SPRING_PROFILES_ACTIVE
               value: 'consul,prod'
+            - name: IDENTITY
+              value: $1
 #            - name: SPRING_CLOUD_CONSUL_HOST
 #              value: 'consul.default'
           readinessProbe:
@@ -59,6 +71,6 @@ spec:
 EOF
 }
 
-for module in bookwarehouse bookstore bookbuyer bookthief; do
+for module in bookwarehouse bookstore bookbuyer bookthief bookstore-v2; do
   generate_yaml $module > ./manifests/$module.yaml
 done
