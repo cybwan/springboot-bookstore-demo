bd41ee487dfe92164901b7d01a6c9cc459e523ae
Addo.Zhang <duwasai@gmail.com>
2023-11-29 11:47:57 +0800

    add feign interceptor to propagate http headers

---
+++ curl/src/main/java/io/flomesh/demo/simple/feign/FeignHeaderPropagationInterceptor.java
@@ -0,0 +1,32 @@
+package io.flomesh.demo.simple.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Configuration
+public class FeignHeaderPropagationInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+        // Get all HTTP headers of the current request
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String headerName = headerNames.nextElement();
+                    String headerValue = request.getHeader(headerName);
+                    // Pass downstream HTTP headers to upstream
+                    template.header(headerName, headerValue);
+                }
+            }
+        }
+    }
+}
