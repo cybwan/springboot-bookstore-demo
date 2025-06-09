66de2167d2e0819d58fd912469ba02bfe1d9c4a1
Cybwan <baili@flomesh.io>
2025-06-09 20:03:38 +0800

    update curl demo

--- curl/src/main/java/io/flomesh/demo/simple/dubbo/CurlController.java
+++ curl/src/main/java/io/flomesh/demo/simple/dubbo/CurlController.java
@@ -9,11 +9,67 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import io.flomesh.demo.api.HttpbinService;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 @Slf4j
 @Profile("dubbo")
 public class CurlController{
+
+    private static final AtomicLong escapeTime = new AtomicLong(0);
+
+    @GetMapping("/")
+    public @ResponseBody String meter(@RequestParam int n,@RequestParam int c) throws InterruptedException {
+        if(n<1){
+            n =1;
+        }
+        if(c<1){
+            c =1;
+        }
+        int totalRequests =n;
+        int concurrencyLevel =c;
+        ExecutorService threadPool = Executors.newFixedThreadPool(concurrencyLevel);
+        CountDownLatch latch = new CountDownLatch(concurrencyLevel);
+
+        // 每个线程执行 (totalRequests / concurrencyLevel) 次请求
+        int requestPerThread = totalRequests / concurrencyLevel;
+        totalRequests =requestPerThread*concurrencyLevel;
+
+        //warmup
+        for(int i=0;i<5;i++){
+            httpbinService.hostname();
+        }
+
+        Thread.sleep(5000); // 等待1秒钟，确保warmup完成
+
+        for (int i = 0; i < concurrencyLevel; i++) {
+            threadPool.execute(() -> {
+                try {
+                    // 记录开始时间
+                    long startTime = System.nanoTime();
+                    for (int j = 0; j < requestPerThread; j++) {
+                        httpbinService.hostname();
+                    }
+                    // 记录结束时间
+                    long  endTime = System.nanoTime();
+                    escapeTime.addAndGet(endTime - startTime);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        // 等待所有线程完成
+        latch.await();
+        threadPool.shutdown();
+        return "totalRequests: " + totalRequests +
+                ", concurrencyLevel: " + concurrencyLevel +
+                ", totalTime: " + escapeTime.get() +
+                " ns, avgTime: " + (escapeTime.get() / totalRequests) + " ns";
+    }
 
     @GetMapping("/")
     public @ResponseBody String index(@RequestParam int count) {
